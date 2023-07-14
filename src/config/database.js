24f39b7ee41a403bc66a55b5e60ca301aa9e5e40
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-07-14 20:39:28 -0300

    build: atualiza variaveis

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,12 @@ import 'dotenv/config';
 
 const dbConfig = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_NAME,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  url: process.env.DB_HOST
 };
 
 export default dbConfig;
