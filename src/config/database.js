69156b0ee979d35a1915c45636de09420944a80a
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-07-15 01:31:25 -0300

    fix: corrige erros de conexão com banco na vercel

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
+import pg from 'pg';
 
 const dbConfig = {
-  dialect: process.env.DB_DIALECT,
+  dialect: "postgres",
+  dialectModule: pg, // I've added this.
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
